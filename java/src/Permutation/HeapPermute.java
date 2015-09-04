ea431a32759a4fe21268fdc17dfa7abc6a094adf
Ap0g33 <saulo@gmx.com>
2015-09-03 21:12:34 -0300

    Adding binary & permutation fixes

--- java/src/Permutation/HeapPermute.java
+++ java/src/Permutation/HeapPermute.java
@@ -2,18 +2,28 @@ package Permutation;
 
 import java.util.Arrays;
 
-public class HeapsPermute {
+public class HeapPermute {
+	
+	public static void main(String[] args) {
+		
+		int[] ns = {1, 2, 3, 4};
+		permute(ns, ns.length);
+	}
 	
 	private static void swap(int[] v, int i, int j) {
-		int t = v[i]; v[i] = v[j]; v[j] = t;
+		int t = v[i];
+		v[i] = v[j];
+		v[j] = t;
 	}
 
-	public void permute(int[] v, int n) {
+	private static void permute(int[] v, int n) {
 		if (n == 1) {
 			System.out.println(Arrays.toString(v));
+			
 		} else {
 			for (int i = 0; i < n; i++) {
 				permute(v, n-1);
+				
 				if (n % 2 == 1) {
 					swap(v, 0, n-1);
 				} else {
--- java/src/Permutation/Permutation.java
+++ java/src/Permutation/Permutation.java
@@ -1,45 +1,32 @@
 package Permutation;
 
+import java.util.Arrays;
+
 public class Permutation {
 
-	private int[] N ;
-    private int n ;
-    private String[] entrada ;
-    private int r ;
- 
-    /**
-     * entrada é seu vetor de elementos e r
-     * é o tamanho da permutacao. Aqui r obrigatoriamente
-     * deve ser maior que zero.
-     */
-    public Permutation(String[] entrada, int r) {
-        this.r = r ;
-        this.n = entrada.length ;
-        this.N = new int[r+1] ;
-        this.entrada = entrada ;
-    }
- 
-    public boolean hasNext() {
-        return this.N[this.r] == 0 ;
-    }
- 
-    public String[] next() {
-        String[] saida = new String[this.r] ;
-        int i, j ;
- 
-        for(i=0, j=this.r-1; i < this.r; i++) {
-            saida[j] = entrada[this.N[i]] ;
-            j-=1 ;
-        }
- 
-        this.N[0] += 1 ;
- 
-        for(i=0; i < this.r; i++) {
-            if(this.N[i] == this.n) {
-                this.N[i] = 0;
-                this.N[i+1] += 1 ;
-            }
-        }
-        return saida ;
-    }
+public static void main(String[] args) {
+		
+		int[] ns = {1, 2, 3, 4};
+		permute(ns, 0, ns.length);
+	}
+	
+	private static void swap(int[] v, int i, int j) {
+		int t = v[i];
+		v[i] = v[j];
+		v[j] = t;
+	}
+
+	private static void permute(int[] v, int start, int n) {
+		if (n == start) {
+			System.out.println(Arrays.toString(v));
+			
+		} else {
+			for (int i = start; i < n; i++) {
+				swap(v, i, start);
+				permute(v, start+1, n);
+				swap(v, i, start);
+			}
+		}
+	}
+
 }
