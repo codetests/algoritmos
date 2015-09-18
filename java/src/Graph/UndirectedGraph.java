5f4722c9fdfe0942c84961bbef0a8a52b2ef2e24
Ap0g33 <saulo@gmx.com>
2015-09-17 21:52:46 -0300

    Fix UndirectedGraph

--- java/src/Graph/UndirectedGraph.java
+++ java/src/Graph/UndirectedGraph.java
@@ -1,8 +1,11 @@
 package Graph;
 
+import java.util.Arrays;
+
 public class UndirectedGraph {
 
 	public static  String adjList[] = new String[15];
+	public static  Integer visited[] = new  Integer[adjList.length];
 	public static  Integer listSize = 0;
 	
 	public static void main(String[] args) {
@@ -10,6 +13,7 @@ public class UndirectedGraph {
 		// Input: [ [0,1], [0,6], [0,8], [1,4], [1,6], [1,9], [2,4], [2,6], [3,4], [3,5], [3,8], [4,5], [4,9], [7,8], [7,9] ]
 		
 		for (int i = 0; i < adjList.length; i++) adjList[i] = "";
+		for (int i = 0; i < visited.length; i++) visited[i] = 0;
 		
 		addVertex(0, "1");
 		addVertex(0, "6");
@@ -29,18 +33,60 @@ public class UndirectedGraph {
 		
 		printGraphList();
 		
+		DFS(0);
+		
 	}
 
+	// Busca em profundidade
+	public static void DFS(int vertex) {
+		
+		visitVertex(vertex);
+		
+		for (String ver : getAdjVertexes(vertex)) {
+			if (!isVisited(Integer.parseInt(ver)))
+				DFS(Integer.parseInt(ver));
+		}		
+	}
+	
+	public static void visitVertex(int pos) {
+		visited[pos] = 1;
+		System.out.println("Visiting: " + pos);
+	}
+	
+	public static Boolean isVisited(int vertex) {
+		if (visited[vertex] == 0)
+			return false;
+		else
+			return true;
+	}
+	
+	public static String[] getAdjVertexes(int vertex) {
+		
+		String list = adjList[vertex];
+		
+		String vector[] = list.split(",");
+		
+//		System.out.println("getVertexes: " + vertex + " returning: " + Arrays.toString(vector));		
+		
+		return vector;
+	}
+	
 	public static void addVertex(int pos, String value) {
 		
 		int posInv = Integer.parseInt(value);
 		
 		adjList[pos] = ("".equals(adjList[pos])) ? "" + value : adjList[pos] + "," + value;
 		adjList[posInv] = ("".equals(adjList[posInv])) ? "" + pos : adjList[posInv] + "," + pos;
+		
+		if (pos > listSize)
+			listSize = pos;
+		
+		if (posInv > listSize)
+			listSize = posInv;		
 	}
 	
 	public static void printGraphList() {
-		for (int i = 0; i < adjList.length; i++)
+		for (int i = 0; i <= listSize; i++)
 			System.out.println(i + ": " + adjList[i]);
 	}
 	
